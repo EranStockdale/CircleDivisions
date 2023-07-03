d27e2808ee4ccaea3bf9d93166af83925b2172b1
Eran Stockdale <eranbd2017@outlook.com>
2023-07-03 19:52:24 +0100

    works properly now

--- main.ts
+++ main.ts
@@ -1,16 +1,10 @@
-function factorial(n: number, depth: number = Infinity): number {
-    return n <= 1 ? 1 : n * factorial(n - 1)
-}
-
-function choose(n: number, k: number): number {
-    return factorial(n) / (factorial(k) * factorial(n - k))
-}
-
 function divisions(pointCount: number): number {
-    return 1 + choose(pointCount, 2) + choose(pointCount, 4)
+    return  1 + 
+            ((pointCount * (pointCount - 1)) / (2 * 1)) + 
+            ((pointCount * (pointCount - 1) * (pointCount - 2) * (pointCount - 3)) / (4 * 3 * 2 * 1))
 }
 
-let pointCount = 0;
+let pointCount = 1;
 while (pointCount < 10) {
     // console.log(`Factorial of ${pointCount}: ${factorial(pointCount)}`)
     console.log(divisions(pointCount))
